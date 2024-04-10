ea416d51a5114b46e89094efbd3760c8f657c6e1
Hima <abrahimmahmoud5454@gmail.com>
2024-04-10 03:48:23 +0200

    si

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
-import requests
+#!/usr/bin/python3
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # Setting a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Custom user agent'}
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    print(number_of_subscribers(subreddit))
+    return sub_info.json().get("data").get("subscribers")
